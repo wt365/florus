572c5d1b32e0b83fd5f4d6ba7f4edb0361730076
Tingyu <wt365@users.noreply.github.com>
2020-11-13 10:05:13 +0800

    支持显示基金估值，更新了天气api无需任何key，支持两种配色方案，支持大中小尺寸，农历已更新

--- florus.js
+++ florus.js
@@ -1,89 +1,119 @@
-;// florus.js v4.6 by Tingyu
-const CD=new Date(), WD=['日','一','二','三','四','五','六'];
-const Con={'CLEAR_DAY':'晴', 'CLEAR_NIGHT':'晴', 'PARTLY_CLOUDY_DAY':'多云', 'PARTLY_CLOUDY_NIGHT':'多云', 'CLOUDY':'阴', 'LIGHT_RAIN':'小雨', 'MODERATE_RAIN':'中雨', 'HEAVY_RAIN':'大雨', 'STORM_RAIN':'暴雨', 'LIGHT_SNOW':'小雪', 'MODERATE_SNOW':'中雪', 'HEAVY_SNOW':'大雪', 'STORM_SNOW':'暴雪', 'FOG':'雾', 'WIND':'大风', 'DUST':'浮尘', 'SAND':'沙尘', 'LIGHT_HAZE':'轻度雾霾', 'MODERATE_HAZE':'中度雾霾', 'HEAVY_HAZE':'重度雾霾',};  // weather text
-const lon=121.234567, lat=31.056789; // 请设置经纬度，用于显示天气
-const apikey='TAkhjf8d1nlSlspN'; // 彩云天气APIKey，公用Key有每小时请求次数限制，如果你有自己的Key，推荐换成自己的。申请地址：https://dashboard.caiyunapp.com/user/sign_in/
-const wAPI='https://api.caiyunapp.com/v2.5/'+apikey+'/'+lon+','+lat+'/realtime.json', yAPI='https://v1.hitokoto.cn/?encode=json'; // weather and motto API
-const max=2, Events=[ // 提醒事项，可添加任意多条，无需按顺序，会自动排序后显示未来最近max条
+;// florus.js v5.0 by Tingyu
+
+// 设置区开始
+const loc='31.223502,121.44532'; // 请设置用于显示天气的位置 // 先纬度，后经度
+const Events=[
+	// 提醒事项，可添加任意多条，无需按时间顺序，将会自动排序
+	// 中尺寸显示未来最近三项，小尺寸（不显示一言）显示未来最近四项，大尺寸显示未来最近六项
+	['参加比赛','2020-12-06'],
 	['新年','2021-01-01'],
 	['东京奥运会','2021-07-23'],
 ];
-let DF=new DateFormatter();
-DF.dateFormat='M月d日'; 
-let datext=DF.string(CD)+' · 周'+WD[CD.getDay()];
-DF.dateFormat='D';
-datext+=' · '+lunar(DF.string(CD))+' · 全年'+Math.floor(Number(DF.string(CD))/(365+(CD.getYear()%4?0:1))*100)+'%';
-let widget=await createWidget(datext,procEvents(Events));
-Script.setWidget(widget);
-Script.complete();
+const FF=0; // 基金功能开关 -> 0:关闭，将正常显示提醒事项 1:打开，中小尺寸用基金实时估值替代提醒事项，大尺寸同时显示提醒事项和基金实时估值
+const Fcodes='004854,000294,150270'; // 请设置基金代码，用英文半角逗号隔开 // 中尺寸显示不超过三个，小尺寸（不显示一言）显示不超过四个，大尺寸显示不超过六个
+const cs=0; // 配色方案 -> 0:黑色调 1:白色调 2:自动切换色调
+// 设置区结束
 
-async function createWidget(da,Ev) {
+const CD=new Date(), size=getSize(), sep=size?' · ':' ';
+let CS=[{b:'#1d1d1d',d:'#fff',w:'#fff59d',e:'#b3e5fc',f:['#b3e5fc','#ffccbc','#c8e6c9'],m:'#fff'},{b:'#eee',d:'#1d1d1d',w:'#424242',e:'#546e8a',f:['#546e7a','#ff5722','#4caf50'],m:'#424242'}];
+CS.push(CD.getHours()>5&&CD.getHours()<18?CS[1]:CS[0]);
+const yola=await createWidget();
+Script.setWidget(yola);
+Script.complete();
+// Functions
+function getSize () {return config.widgetFamily=='large'?2:config.widgetFamily=='medium'?1:0;}
+async function createWidget() {
 	let w= new ListWidget()
-	w.backgroundColor=new Color('#111',0.9);
-	// Dateline
-	let date=w.addText(da);
-	date.font=Font.boldSystemFont(15);
-	date.textColor=Color.white();
-	w.addSpacer(8);
+	w.backgroundColor=new Color(CS[cs].b,0.9);
+	// Date
+	let date=w.addText(getDatext());
+	date.font=Font.boldSystemFont(14), date.textColor=new Color(CS[cs].d);
 	// Weather
-	let wtext='',rb=10000;
-	try {
-		const reqW=new Request(wAPI), resW=await reqW.loadJSON()||null, rt=resW.result.realtime||null, rn=rt.precipitation.nearest||null;
-		if (rn) {rb=rn.distance>10?Math.floor(rn.distance):rn.distance;}
-		wtext+=Con[rt.skycon]+' · '+rt.temperature+'度';
-		if (rb<100) {wtext+=' · 最近降雨带'+rb+ '公里外';}
-	} catch (err) {
-		wtext='暂时与气象卫星失去了联系';
-	}
+	w.addSpacer(6);
+	let wtext='';
+	try {wtext=await getWeather(loc);} catch (e) {wtext='暂时与气象卫星失去了联系';}
 	let weather=w.addText(wtext);
-	weather.font=Font.lightSystemFont(13);
-	weather.textColor=new Color('#fff59d',0.9);
-	weather.textOpacity=0.9;
-	w.addSpacer(2);
+	weather.font=Font.lightSystemFont(12), weather.textColor=new Color(CS[cs].w,0.9), weather.textOpacity=0.9;
 	// Events
-	for (let o of Ev) {
-		w.addSpacer(6); // newline
-		let ev=w.addText(o);
-		ev.font=Font.lightSystemFont(11);
-		ev.textColor=new Color('#b3e5fc',0.9);
-		ev.textOpacity=0.8;
+	if (FF==0||size>1) {
+		w.addSpacer(2);
+		const Ev=procEvents(Events);
+		for (let o of Ev) {
+			w.addSpacer(4);
+			let event=w.addText(o);
+			event.font=Font.lightSystemFont(11), event.textColor=new Color(CS[cs].e,0.9), event.textOpacity=0.8;
+		}
 	}
-	w.addSpacer(8);
+	// Funds
+	if (FF==1) {
+		w.addSpacer(2);
+		const Fu=await getFunds();
+		for (let o of Fu) {
+			w.addSpacer(4);
+			let fund=w.addText(o[0]);
+			fund.font=Font.lightSystemFont(11), fund.textColor=new Color(CS[cs].f[o[1]],0.9), fund.textOpacity=0.8;
+		}
+	}
 	// Motto
-	let y='';
-	try {
-		const reqY=new Request(yAPI), resY=await reqY.loadJSON()||null;
-		y+='“'+resY.hitokoto+'” -- '+resY.from;
-	} catch (err) {
-		y='“此时无声胜有声。” -- 在那个没有互联网的远古时代';
+	if (size) {
+		w.addSpacer(6);
+		let y='';
+		try {y=await getMotto();} catch (e) {y='“此时无声胜有声。” -- 在那个没有互联网的远古时代';}
+		let motto=w.addText(y);
+		motto.font=Font.lightSystemFont(10), motto.textColor=new Color(CS[cs].m,0.9), motto.textOpacity=0.8;
 	}
-	let motto=w.addText(y);
-	motto.font=Font.lightSystemFont(10);
-	motto.textColor=new Color('#fff',0.9);
-	motto.textOpacity=0.8;
 	// return widget
 	return w;
 }
-
-function procEvents(E) {
+function getDatext () {
+	let DF=new DateFormatter();
+	DF.dateFormat='M月d日'; 
+	let da=DF.string(CD)+sep+'周'+['日','一','二','三','四','五','六'][CD.getDay()];
+	DF.dateFormat='D';
+	if (size) {da+=sep+lunar(DF.string(CD))+sep+'全年'+Math.floor(DF.string(CD)/(365+(CD.getYear()%4?0:1))*100)+'%';}
+	return da;
+}
+async function getWeather (loc) {
+	const req=new Request('http://wttr.in/'+loc+'?format=j1&lang=zh'), du=size?'度 ':'°', dux=size?'度':'';
+	req.allowInsecureRequest=true;
+	const res=await req.loadJSON()||null, CC=res.current_condition[0]||null, W=res.weather[0]||null;
+	return CC.lang_zh[0].value+sep+CC.temp_C+du+' ('+(size?'最低':'')+(CC.temp_C<W.mintempC?CC.temp_C:W.mintempC)+dux+(size?' / ':'/')+(size?'最高':'')+(CC.temp_C>W.maxtempC?CC.temp_C:W.maxtempC)+dux+')';
+}
+function procEvents (E) {
+	const max=size>1?6:size?3:4, xday=size?' · 还有':' ', today=size?' · 就是今天':'今天';
 	E.sort((a,b)=>{return new Date(a[1]).getTime()-new Date(b[1]).getTime();});
 	let Ex=[];
 	for (let o of E) {
 		let diff=Math.ceil((new Date(o[1]).getTime()-28800000-CD.getTime())/86400000);
-		if (diff>-1) {Ex.push(o[0]+(diff>0?' · 还有'+diff+'天':' · 就是今天')); if (Ex.length==max) {break;}}
+		if (diff>-1) {Ex.push(o[0]+(diff>0?xday+diff+'天':today)); if (Ex.length==max) {break;}}
 	}
 	return Ex;
 }
-
-function lunar(D) {
-	// 农历暂支持2020年，到了2021年我再更新
-	const L=[-6,24,53,83,113,143,172,202,231,260,290,319,349], Lx=['腊','正','二','三','四','闰四','五','六','七','八','九','十','冬'];
-	let i=L.length-1;
-	while (D<L[i]+1) {i--;}
-	const lm=Lx[i], ld=D-L[i];
-	return lm+'月'+zh(ld);
+async function getFunds () {
+	const max=size>1?6:size?3:4;
+	let Fx=[];
+	try {
+		const req=new Request('https://api.doctorxiong.club/v1/fund?code='+Fcodes), res=await req.loadJSON()||{};
+		let F=res.data||[];
+		if (F.length>max) {F.length=max;}
+		for (let o of F) {
+			let g=o.expectGrowth;
+			Fx.push([(size?o.name:o.code)+' · '+o.expectWorth+(size?' ('+(g>0?'+':'')+g+'%)':''), g>0?1:g<0?2:0]);
+		}
+	} catch (e) {Fx=[['暂时与交易所失去了联系',0]];}
+	return Fx;
 }
-
+async function getMotto () {
+	const req=new Request('https://v1.hitokoto.cn/?encode=json'), res=await req.loadJSON()||null;
+	return '“'+res.hitokoto+'” -- '+res.from;
+} 
+function lunar (D) {
+	// 农历已支持到2021年，到了2022年我再更新
+	const y=CD.getFullYear(), L={'2020': [-6,24,53,83,113,143,172,202,231,260,290,319,349], '2021': [-17,12,42,71,101,131,160,190,219,249,278,308,337]}, Lx={'2020': ['腊','正','二','三','四','闰四','五','六','七','八','九','十','冬'], '2021': ['冬','腊','正','二','三','四','五','六','七','八','九','十','冬']};
+	let i=12;
+	while (D<L[y][i]+1) {i--;}
+	return Lx[y][i]+'月'+zh(D-L[y][i]);
+}
 function zh(x) {
 	const Z=['','一','二','三','四','五','六','七','八','九','十'];
 	return x<11?'初'+Z[x]:x<20?'十'+Z[x-10]:x==20?'二十':x<30?'廿'+Z[x-20]:'三十';
